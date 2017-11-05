9297eb0d8f69b8fa48e376d5b0a81d8df8a285a3
MartR <juneyourtech@users.noreply.github.com>
2017-11-05 12:55:22 +0200

    Update Rddit_Rez.user.js
    Adjusted font to Arial, and fond size.

--- Rddit_Rez.user.js
+++ Rddit_Rez.user.js
@@ -3,8 +3,11 @@
 // @namespace   Rddit_Rez
 // @description Fit the Reddit comment area to the sidebar. This should adjust the comment area width on smaller-resolution screens (1024x768px)
 // @include     https://www.reddit.com/*
-// @version     0.1
+// @version     0.1.1
 // @grant       GM_addStyle
 // ==/UserScript==
 
 GM_addStyle("DIV.sitetable {display:table-cell;}");
+
+/* comment */
+GM_addStyle("DIV.md, DIV.md P {font-family:\'Arial\',\'Helvetica\',\'Helv\',\'sans-serif\',\'sans\'; font-size:100%}");
