37aa2d0c0b390b0e2663926528d429b0194cf693
MartR <juneyourtech@users.noreply.github.com>
2017-11-05 12:58:12 +0200

    Update Rddit_Rez.user.js

--- Rddit_Rez.user.js
+++ Rddit_Rez.user.js
@@ -3,11 +3,11 @@
 // @namespace   Rddit_Rez
 // @description Fit the Reddit comment area to the sidebar. This should adjust the comment area width on smaller-resolution screens (1024x768px)
 // @include     https://www.reddit.com/*
-// @version     0.1.1
+// @version     0.1.2
 // @grant       GM_addStyle
 // ==/UserScript==
 
 GM_addStyle("DIV.sitetable {display:table-cell;}");
 
 /* comment */
-GM_addStyle("DIV.md, DIV.md P {font-family:\'Arial\',\'Helvetica\',\'Helv\',\'sans-serif\',\'sans\'; font-size:100%}");
+GM_addStyle("DIV.md, DIV.md P {font-family:\'Arial\',\'Helvetica\',\'Helv\',\'sans-serif\',\'sans\'; font-size:100%; line-height:1.5em;}");
