75293ac63b4956fe2fe82e37aafff1a0d49461cc
MartR <juneyourtech@users.noreply.github.com>
2019-09-12 02:04:19 +0300

    Update NRJ.user.js

--- Eesti/NRJ.user.js
+++ Eesti/NRJ.user.js
@@ -2,11 +2,11 @@
 // @name        NRJ
 // @namespace   nrj
 // @description Turn off background animation resource hog
-// @updateURL https://github.com/juneyourtech/varia/raw/master/NRJ.user.js
+// @updateURL https://github.com/juneyourtech/varia/raw/master/Eesti/NRJ.user.js
 // @include     http://nrj.ee/*
 // @include     http://player.nrj.ee/*
-// @version     1.0.0.1
+// @version     1.0.0.2
 // @grant       GM_addStyle
 // ==/UserScript==
 
-GM_addStyle("BODY.Site {animation:none;}") //Removes animation
+GM_addStyle("BODY.Site {animation:none;}"); /* Removes animation */
